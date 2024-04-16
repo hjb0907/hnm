916122c650de11f3a366145eda81c5d21f3a6ad0
[웹개발2팀]봉효정 <hyohyo2907@gmail.com>
2024-04-16 09:10:54 +0900

    오류 수정

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser as solidUser, faUserRegular as regularUser } from '@fortawesome/free-regular-svg-icons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/hnm'>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
