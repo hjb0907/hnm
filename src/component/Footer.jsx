abb506788fa6b5a2d8e7f11f282b6e2c7016aebc
[웹개발2팀]봉효정 <hyohyo2907@gmail.com>
2024-04-19 14:57:23 +0900

    ui 수정

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -31,24 +31,30 @@ const Footer = () => {
           <li>
             <h5>카테고리</h5>
           </li>
-          {list[0].row.map((category, idx) => (
-            <a href={list[0].link[idx]} key={idx}>{category}</a>
-          ))}
+          <ul>
+            {list[0].row.map((category, idx) => (
+              <li key={idx}><a href={list[0].link[idx]} >{category}</a></li>
+            ))}
+          </ul>
         </ul>
         <ul className="row02">
           <li>
             <h5>기업 정보</h5>
-            {list[1].row.map((category, idx) => (
-              <a href={list[1].link[idx]}  key={idx}>{category}</a>
-            ))}
+            <ul>
+              {list[1].row.map((category, idx) => (
+                <li key={idx}><a href={list[1].link[idx]}  >{category}</a></li>
+              ))}
+            </ul>
           </li>
         </ul>
         <ul className="row03">
           <li>
             <h5>고객지원</h5>
-            {list[2].row.map((category, idx) => (
-              <a href={list[2].link[idx]} key={idx}>{category}</a>
-            ))}
+            <ul>
+              {list[2].row.map((category, idx) => (
+                <li key={idx}><a href={list[2].link[idx]}>{category}</a></li>
+              ))}
+            </ul>
           </li>
         </ul>
         <ul className="row04">
@@ -59,26 +65,38 @@ const Footer = () => {
             <p>지금 가입하시고 10% 할인받으세요</p>
           </li>
           <li>
-            <Link to='/'>지금 가입하기</Link>
+            <Link to='/login'>지금 가입하기</Link>
             <FontAwesomeIcon icon={faArrowRight} />
           </li>
         </ul>
       </div>
       <div className="col02">
-        <FontAwesomeIcon icon={faInstagram} />
-        <FontAwesomeIcon icon={faTiktok} />
-        <FontAwesomeIcon icon={faYoutube} />
-        <FontAwesomeIcon icon={faPinterest} />
-        <FontAwesomeIcon icon={faFacebook} />
-        <FontAwesomeIcon icon={faCommentSolid} />
+        <Link to='/'>
+          <FontAwesomeIcon icon={faInstagram} />
+        </Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faTiktok} />
+        </Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faYoutube} />
+        </Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faPinterest} />
+        </Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faFacebook} />
+        </Link>
+        <Link to='/'>
+          <FontAwesomeIcon icon={faCommentSolid} />
+        </Link>
       </div>
       <div className="col03">
-      이 사이트의 콘텐츠는 저작권 보호를 받고 있는 H & M Hennes & Mauritz AB의 자산입니다. 더 보기 | 법인명 에이치앤엠헤네스 앤 모리츠 주식<br/>
-      회사 | 통신판매업신고번호 : 2022-서울강남-01184 / 사업자등록| 번호: 220-87-83339 | 대표자: 아담 칼슨, 선 보라미, 아네타 포쿠친스카 <br/>
-      서울특별시 강남구 영동대로 421, 9층 삼탄빌딩 (대치동) 06182 | 대표번호 080-822-0220 info.kr@hm.com <br/>사업자 정보 확인 지급보증안내 법인명 에이치앤엠헤네스앤모리츠 주식회사 | 통신판매업신고번호 : 2022-서울강남-01184 / 사업자등록번호: 220-87-83339 | 아담 칼슨, 선 보라미, 아네타 포쿠친스카 | 서울특별시 강남구 영동대로 421, 9층 삼탄빌딩 (대치동) 06182 | 대표번호 080-822-0220 | info.kr@hm.com
-      <br/>
-      사업자 정보 확인
-      지급보증안내
+      <p>이 사이트의 콘텐츠는 저작권 보호를 받고 있는 H & M Hennes & Mauritz AB의 자산입니다. 더 보기 | 법인명 에이치앤엠헤네스 앤 모리츠 주식</p>
+      <p>회사 | 통신판매업신고번호 : 2022-서울강남-01184 / 사업자등록| 번호: 220-87-83339 | 대표자: 아담 칼슨, 선 보라미, 아네타 포쿠친스카</p>
+      <p>서울특별시 강남구 영동대로 421, 9층 삼탄빌딩 (대치동) 06182 | 대표번호 080-822-0220 info.kr@hm.com </p>
+      <p>사업자 정보 확인 지급보증안내 법인명 에이치앤엠헤네스앤모리츠 주식회사 | 통신판매업신고번호 : 2022-서울강남-01184 / 사업자등록번호: 220-87-83339 | 아담 칼슨, 선 보라미, 아네타 포쿠친스카 | 서울특별시 강남구 영동대로 421, 9층 삼탄빌딩 (대치동) 06182 | 대표번호 080-822-0220 | info.kr@hm.com</p>
+      <p>사업자 정보 확인</p>
+      <p>지급보증안내</p>
       </div>
       <div className="col04">
         <div className="logo" onClick={gotoHome}>
